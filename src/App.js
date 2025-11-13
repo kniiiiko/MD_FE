29a253fea333fcd730c405f8b5bd2598276cb990
hahee <hahee_hi@naver.com>
2025-11-14 00:44:30 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import React from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
